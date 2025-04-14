7da5f33b893b4273f2dd5ff969ab85b3b9a94ae5
atnychi <ksystemsandsecurities@proton.me>
2025-04-14 13:39:02 -0500

    Create Juanita_core.py

---
+++ src/Juanita_core.py
@@ -0,0 +1,10 @@
+# Core recursive encryption engine
+
+class JuanitaCore:
+    def encrypt(self, data, key):
+        # Placeholder for recursive encryption (simple reverse)
+        return data[::-1]
+
+    def decrypt(self, data, key):
+        # Placeholder for recursive decryption (simple reverse)
+        return data[::-1]
